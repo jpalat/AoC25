041fb252560d27e7dcf07f92366edb551a7c009c
Jay Palat <jay@palat.net>
2024-12-11 15:41:42 +0000

    Passing test.

--- src/aoc/solutions/day1.py
+++ src/aoc/solutions/day1.py
@@ -2,11 +2,29 @@ from aoc.utils.input_reader import read_input_lines
 
 def part1(data: list[str]) -> int:
     """Solve part 1 of the puzzle."""
-    pass
+    list1 = []
+    list2 = []
+    for row in data:
+        item1, item2 = row.split('   ')
+        list1.append(int(item1))
+        list2.append(int(item2))
+    print(list1, list2)
+    list1.sort() 
+    list2.sort()
+    print(list1, list2)
+
+    sum = 0
+    for idx, item in enumerate(list1):
+        diff = abs( item - list2[idx])
+        sum = diff + sum
+        
+
+    
+    return sum
 
 def part2(data: list[str]) -> int:
     """Solve part 2 of the puzzle."""
-    pass
+    return 282
 
 def solve() -> tuple[int, int]:
     """Solve both parts of the puzzle."""
