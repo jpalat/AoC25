d6aad37f377dc75fe12f283568348f953f712c13
Jay Palat <jay@palat.net>
2024-12-11 15:33:53 +0000

    Add initial test.

--- tests/test_solutions/test_day1.py
+++ tests/test_solutions/test_day1.py
@@ -4,21 +4,23 @@ from aoc.solutions.day1 import part1, part2
 
 def test_part1():
     test_input = [
-        "1abc2",
-        "pqr3stu8vwx",
-        "a1b2c3d4e5f",
-        "treb7uchet"
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3"
     ]
-    assert part1(test_input) == 142
+    assert part1(test_input) == 11
 
-def test_part2():
-    test_input = [
-        "two1nine",
-        "eightwothree",
-        "abcone2threexyz",
-        "xtwone3four",
-        "4nineeightseven2",
-        "zoneight234",
-        "7pqrstsixteen"
-    ]
-    assert part2(test_input) == 281
+# def test_part2():
+#     test_input = [
+#         "two1nine",
+#         "eightwothree",
+#         "abcone2threexyz",
+#         "xtwone3four",
+#         "4nineeightseven2",
+#         "zoneight234",
+#         "7pqrstsixteen"
+#     ]
+#     assert part2(test_input) == 281
