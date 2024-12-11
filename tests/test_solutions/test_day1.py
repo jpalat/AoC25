c0b8841bff5b86675d3d23842a07161878df93d5
Jay Palat <jay@palat.net>
2024-12-11 15:45:15 +0000

    Add test for part 2.

--- tests/test_solutions/test_day1.py
+++ tests/test_solutions/test_day1.py
@@ -13,14 +13,13 @@ def test_part1():
     ]
     assert part1(test_input) == 11
 
-# def test_part2():
-#     test_input = [
-#         "two1nine",
-#         "eightwothree",
-#         "abcone2threexyz",
-#         "xtwone3four",
-#         "4nineeightseven2",
-#         "zoneight234",
-#         "7pqrstsixteen"
-#     ]
-#     assert part2(test_input) == 281
+def test_part2():
+    test_input = [
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3"
+    ]
+    assert part2(test_input) == 31
