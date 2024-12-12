4fc474bbca64cbb1507244160fef5e0ac5fbeae2
Jay Palat <jay@palat.net>
2024-12-12 04:08:45 +0000

    Part 2 solution.

--- src/aoc/solutions/day2.py
+++ src/aoc/solutions/day2.py
@@ -1,4 +1,5 @@
 from aoc.utils.input_reader import read_input_lines
+import copy
 
 def check_state(reports: list[str]) -> bool:
 
@@ -23,8 +24,6 @@ def check_state(reports: list[str]) -> bool:
                     return False
     return True
 
-
-
 def part1(data: list[str]) -> int:
     """Solve part 1 of the puzzle."""
     status_list = []
@@ -41,11 +40,36 @@ def part1(data: list[str]) -> int:
             print(safe_count)
     return safe_count
 
+def problem_dampener(data: list[str]) ->bool:
+    for i in range(0, len(data)):
+        pd_list = copy.deepcopy(data)
+        pd_list.pop(i)
+        if check_state(pd_list):
+            return True
+    return False
+
 
 
 def part2(data: list[str]) -> int:
     """Solve part 2 of the puzzle."""
-    pass
+    status_list = []
+    report_list = []
+    for row in data:
+        reports = row.split(' ')
+        status = check_state(reports)
+        status_list.append(status)
+        report_list.append(reports)
+    
+    safe_count = 0
+    print(status_list)
+    for index, status in enumerate(status_list):
+        if status:
+            safe_count = safe_count + 1
+            print(safe_count)
+        else:
+            if problem_dampener(report_list[index]):
+                safe_count = safe_count + 1
+    return safe_count
 
 def solve() -> tuple[int, int]:
     """Solve both parts of the puzzle."""
